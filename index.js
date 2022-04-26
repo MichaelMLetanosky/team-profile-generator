8bc2c8008eda7dbc6a67657f8583e0b438e80a50
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-04-25 21:59:10 -0400

    Updated classes with role data

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const fs = require(`fs`);
 const inquirer = require(`inquirer`);
 const path = require(`path`);
 const { exit } = require("process");
-const Manager = require('./lib/manager');
-const Engineer = require('./lib/engineer');
-const Intern = require('./lib/intern');
+// const Manager = require('./lib/manager');
+// const Engineer = require('./lib/engineer');
+// const Intern = require('./lib/intern');
 let gatheredData = [];
 
-// const generateMarkdown = require(`./utils/generateMarkdown`);
 
-// An array of questions for user input
+// An array of roles
 const roleQuestion = [{
     name: `role`, type: `list`,
     message: `I would like to add a...`,
-    choices: [`Manager`, `Engineer`, `Intern`, `None, I'm finished`],
+    choices: [`Engineer`, `Intern`, `No, I'm finished`],
 }]
 
+// Questions to gather data based on role
 const employeeQuestions = [{
     name: `employeeName`, type: `input`,
     message: `What is their name?`,
@@ -26,6 +26,14 @@ const employeeQuestions = [{
     },{
     name: `email`, type: `input`,
     message: `What is their email?`,
+    },{
+    name: `officeNumber`, type: `input`,
+    message: `What is their officeNumber?`,
+    when: (answers) => {
+        if (answers.role === `Manager`) {
+            return true;
+        };
+    }
     },{
     name: `github`, type: `input`,
     message: `What is their gitHub username?`,
@@ -44,6 +52,41 @@ const employeeQuestions = [{
     }
 }]
 
+// Initial function that starts gathering info about the project manager
+function init() {
+    console.log(`Let's start with a manager!`)
+    // Sets the first input team members role to be the only Manager
+    gatherData({ role: 'Manager' })
+}
+
+// Function that gathers info about the chosen employee
+function gatherData(role) {
+  inquirer
+    .prompt(employeeQuestions, role)
+    .then((answers) =>{
+        // Gets an object for the employee and adds to an array
+        gatheredData = gatheredData.concat([answers]);
+        console.log(gatheredData)
+        addEmployee();
+    })
+    .catch((e) => {if (e) throw e;});
+};
+
+// Allows the user to either select they are done or to add additional employees
+function addEmployee() {
+  console.log (`How about another employee?`)
+  inquirer
+    .prompt(roleQuestion)
+    .then((answers) => {
+        if (answers.role === `No, I'm finished`) {
+            buildPage(gatheredData);
+        };
+        gatherData(answers);
+    })
+    .catch((e) => {if (e) throw e;});
+};
+
+
 // // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {
 //     fs.writeFile(`./output/${fileName}`, generateMarkdown(data), (e) => e ? console.log(e) : console.log(`Success! File is in Output Folder`))
@@ -53,22 +96,6 @@ const employeeQuestions = [{
 // TODO: Create a function to initialize app
 // Starts the inquirer to prompt answers to feed to the writeToFile function
 
-function addEmployee() {
-  inquirer
-    .prompt(roleQuestion)
-    .then((answers) => {
-        if (answers.role === `None, I'm finished`) {
-            buildPage();
-        };
-        gatherInfo(answers);
-    })
-    .catch((e) => {if (e) throw e;});
-};
-
-function gatherInfo(role) {
-    console.log(role)
-    addEmployee()
-};
 
 // Function call to initialize app
-addEmployee();
+init();
